729ec173032a99561785b76f3f62c185b48b4e71
Sam Dillard <sam@influxdata.com>
2021-12-21 21:09:55 -0800

    pass: add test for using pipe operator in rule as well

--- parse_test.go
+++ parse_test.go
@@ -27,6 +27,27 @@ func TestTokenizationRule(t *testing.T) {
 		assertEqualRuleMaps(t, gotRuleMap, wantedRuleMap)
 
 	})
+	t.Run("test parsing rule to map with pipe op", func(t *testing.T) {
+		rule := "MEASUREMENT>region>host|app>FIELD"
+		wantedRuleMap := &RuleMap{
+			[]Word{
+				{"MEASUREMENT", 0},
+				{"region", 2},
+				{"host", 4},
+				{"app", 6},
+				{"FIELD", 8},
+			},
+			[]Op{
+				{">", 1},
+				{">", 3},
+				{"|", 5},
+				{">", 7},
+			},
+		}
+		gotRuleMap := ruleTokenizer.Tokenize(rule)
+		assertEqualRuleMaps(t, gotRuleMap, wantedRuleMap)
+
+	})
 }
 
 func assertEqualRuleMaps(t testing.TB, got, want *RuleMap) {
