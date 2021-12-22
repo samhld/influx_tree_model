ddf0be69c869da6c5baea6197110d12fbd2744f0
Sam Dillard <sam@influxdata.com>
2021-12-21 20:40:00 -0800

    pass: RuleTokenizer and Tokenize method successfully parse first rule into an map with each element indexed

--- parse.go
+++ parse.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"regexp"
 	"strings"
-	"unicode"
 )
 
-type Rule struct {
-	line             string
-	measurementIndex int
-	tagsIndices      []int
-	fieldsIndices    []int
+type RuleTokenizer struct {
+	line string
+	re   *regexp.Regexp
+}
+
+type RuleMap struct {
+	words []Word
+	ops   []Op
+}
+
+type Word struct {
+	text  string
+	index int
+}
+
+type Op struct {
+	text  string
+	index int
 }
 
 type Line struct {
@@ -34,11 +47,25 @@ type Field struct {
 type Tags map[string]string
 type Fields map[string]interface{}
 
-tokenizeRule(userRule string) {
-	f := func(c rune) bool {
-		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+func NewRuleTokenizer(line string) *RuleTokenizer {
+	return &RuleTokenizer{
+		line,
+		regexp.MustCompile(`(?P<words>[a-z A-Z _]+)|(?P<ops>[|>\s])`),
 	}
-	strings.FieldsFunc(line, f)
+}
+func (t *RuleTokenizer) Tokenize(rule string) *RuleMap {
+	matches := t.re.FindAllStringSubmatch(rule, -1)
+	ruleMap := &RuleMap{}
+	for i, match := range matches {
+		if match[1] != "" { // 2nd position is a 'word'
+			word := Word{match[1], i}
+			ruleMap.words = append(ruleMap.words, word)
+		} else {
+			op := Op{match[2], i}
+			ruleMap.ops = append(ruleMap.ops, op)
+		}
+	}
+	return ruleMap
 }
 
 func MeasIndex(line string) {
