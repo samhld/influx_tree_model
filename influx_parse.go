78c4250774f8c9be7856b3180d74c587fa37f612
Sam Dillard <sam@influxdata.com>
2021-12-23 12:26:46 -0800

    pass: in and out of order cardinality maps are correctly sorted

--- influx_parse.go
+++ influx_parse.go
@@ -1,11 +1,39 @@
 package main
 
+import (
+	"sort"
+)
+
 func sortByCardinality(cardMap map[string]int) []string {
 	// var cardsToBeSorted []int
-	var tagsSorted []string
-	// for k, v := cardMap {
 
-	// }
-	// sort.Ints(cards)
+	var pairs TagCardPairList
+
+	for k, v := range cardMap {
+		pair := TagCardPair{k, v}
+		pairs = append(pairs, pair)
+	}
+	sort.Sort(pairs)
+
+	var tagsSorted []string
+	for _, pair := range pairs {
+		tagsSorted = append(tagsSorted, pair.Tag)
+	}
+
 	return tagsSorted
 }
+
+type TagCardPair struct {
+	Tag         string
+	Cardinality int
+}
+
+type TagCardPairList []TagCardPair
+
+// implement Sort interface on TagPairList
+// --------------------------------------------
+func (p TagCardPairList) Len() int           { return len(p) }
+func (p TagCardPairList) Less(i, j int) bool { return p[i].Cardinality < p[j].Cardinality }
+func (p TagCardPairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// ----------------------------------------------------
--- influx_parse_test.go
+++ influx_parse_test.go
@@ -5,15 +5,28 @@ import (
 )
 
 func TestOrdering(t *testing.T) {
-	cardMap := map[string]int{
-		"tag1": 4,
-		"tag2": 3,
-		"tag3": 5,
-		"tag4": 1,
-	}
-	gotSortedTags := sortByCardinality(cardMap)
-	wantSortedTags := []string{"tag4", "tag2", "tag1", "tag3"}
+	t.Run("test sorting out-of-order", func(t *testing.T) {
+		cardMap := map[string]int{
+			"tag1": 4,
+			"tag2": 3,
+			"tag3": 5,
+			"tag4": 1,
+		}
+		gotSortedTags := sortByCardinality(cardMap)
+		wantSortedTags := []string{"tag4", "tag2", "tag1", "tag3"}
 
-	assertEqual(t, gotSortedTags, wantSortedTags)
+		assertEqual(t, gotSortedTags, wantSortedTags)
+	})
+	t.Run("test return already-sorted", func(t *testing.T) {
+		cardMap := map[string]int{
+			"tag1": 1,
+			"tag2": 2,
+			"tag3": 3,
+			"tag4": 4,
+		}
+		gotSortedTags := sortByCardinality(cardMap)
+		wantSortedTags := []string{"tag1", "tag2", "tag3", "tag4"}
 
+		assertEqual(t, gotSortedTags, wantSortedTags)
+	})
 }
