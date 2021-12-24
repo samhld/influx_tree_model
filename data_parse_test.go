7eeb295fd213807c93d2156b372385609cf9df79
Sam Dillard <sam@influxdata.com>
2021-12-23 17:30:43 -0800

    fail: createKeyValuesMap func not created yet

--- data_parse_test.go
+++ data_parse_test.go
@@ -3,4 +3,23 @@ package main
 import "testing"
 
 func TestParse(t *testing.T) {
+	tagKeys := []string{"key1", "key2", "key3"}
+	key1Values := []string{"val1", "val2", "val3"}
+	key2Values := []string{"val1", "val2"}
+	key3Values := []string{"val1", "val2", "val3", "val4"}
+
+	t.Run("test creating map of keys to values", func(t *testing.T) {
+		wantMap := map[string][]string{
+			"key1": []string{"val1", "val2", "val3"},
+			"key2": []string{"val1", "val2"},
+			"key3": []string{"val1", "val2", "val3", "val4"},
+		}
+
+		gotMap := createKeyValuesMap(tagKeys)
+
+		assertEqual(t, gotMap, wantMap)
+	})
+	// t.Run("test ordering tags by cardinality", func(t *testing.T) {
+
+	// }
 }
